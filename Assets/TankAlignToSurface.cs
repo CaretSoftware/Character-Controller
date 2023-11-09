b854541111eea69ba4900112eb12951855159f80
CaretSoftware <69549081+CaretSoftware@users.noreply.github.com>
2023-11-09 03:56:21 +0100

    Tank Sounds

--- Assets/TankAlignToSurface.cs
+++ Assets/TankAlignToSurface.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class TankAlignToSurface : MonoBehaviour {
@@ -15,43 +11,41 @@ public class TankAlignToSurface : MonoBehaviour {
     [SerializeField] private float smoothTime = .5f;
     private Vector3 normal;
     private Vector3 currentVelocity;
-
-    private void Start() {
-        //tankTransform.parent = null;
-    }
-
-    public void Update() {
+    
+    public void Update() => AlignToSurface();
+    
+    private void AlignToSurface() {
         RayCastToSurface(out Vector3 surfaceNormal);
         
-        Physics.SphereCast(transform.position + Vector3.up * 3.1f, 3f, Vector3.down, out RaycastHit hit, 3f, layerMask);
+        Physics.SphereCast(transform.position + Vector3.up * 3.1f, 3f, Vector3.down, 
+                out RaycastHit hit, 3f, layerMask);
         normal = Vector3.SmoothDamp(normal, surfaceNormal, ref currentVelocity, smoothTime);
         
         var fwd = Vector3.ProjectOnPlane(tankParentTransform.forward, normal);
         tankTransform.rotation = Quaternion.LookRotation(fwd, normal);
     }
-
-    private bool RayCastToSurface(out Vector3 surfaceNormal) {
+    
+    private void RayCastToSurface(out Vector3 surfaceNormal) {
         raycastHits.Clear();
-        
         for (int i = 0; i < raycastPoints.Length; i++)
             raycastHits.Add(RayHit(tankTransform.TransformPoint(raycastPoints[i])));
         
-        if (raycastHits.Count < 4) {
+        if (raycastHits.Count < 4)
             surfaceNormal = normal;
-            return false;
-        }
         
         raycastHits.Sort((Vector3 a, Vector3 b) => {
-            if (a.y == b.y) return 0;
+            if (a.y == b.y) 
+                return 0;
             return a.y > b.y ? -1 : 1;
         });
         
+        // Remove lowest point to guarantee a flat plane (triangle)
         raycastHits.RemoveAt(raycastHits.Count - 1);
 
         // Calculate vectors lying on the plane
         Vector3 U = raycastHits[1] - raycastHits[0];
         Vector3 V = raycastHits[2] - raycastHits[0];
-
+        
         // Calculate the surface normal
         surfaceNormal = Vector3.Cross( U, V);
         surfaceNormal.Normalize();
@@ -59,28 +53,24 @@ public class TankAlignToSurface : MonoBehaviour {
         // Vectors might be clockwise, invert surfaceNormal if it is
         if (surfaceNormal.y < 0f)
             surfaceNormal = -surfaceNormal;
-        
-        return true;
     }
-
+    
     private Vector3 RayHit(Vector3 position) {
-        if (Physics.SphereCast(position + (radius + 0.1f) * Vector3.up, radius, Vector3.down, out RaycastHit hit, 3f, layerMask)) {
+        if (Physics.SphereCast(position + (radius + 0.1f) * Vector3.up, radius, Vector3.down, 
+                    out RaycastHit hit, 3f, layerMask)) {
             return hit.point;
         }
-
         
         return Vector3.zero;
     }
     
-    private void OnDrawGizmos() {
-        //Gizmos.color = Color.green;
-        //foreach (var vector in raycastPoints) {
-        //    Gizmos.DrawWireSphere(tankTransform.TransformPoint(vector), 1.18f);
-        //}
-
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.green;
+        foreach (var vector in raycastPoints)
+            Gizmos.DrawWireSphere(tankTransform.TransformPoint(vector), 1.18f);
+        
         Gizmos.color = Color.red;
-        foreach (var vector in raycastHits) {
+        foreach (var vector in raycastHits)
             Gizmos.DrawWireSphere(vector + radius * Vector3.up, radius);
-        }
     }
 }
---
+++ Assets/TankAudioEvents.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class TankAudioEvents : MonoBehaviour {
+    [SerializeField] private AudioSource startSource;
+    [SerializeField] private AudioSource idleSource;
+    [SerializeField] private AudioClip tractorStart;
+    [SerializeField] private float engineWindDownTime = .3f;
+    [SerializeField] private float engineThrottlePitch = 1.2f;
+    private Character.IInput input;
+    private CharacterController characterController;
+    
+    private double tractorStartDuration;
+    private double idleStartTime;
+    
+    public void TankHatchDown(AnimationEvent e) => SoundManager.PlaySound(Sound.ToiletSeatDown, .5f);
+    
+    public void TankHatchUp(AnimationEvent e) => SoundManager.PlaySound(Sound.ToiletSeatUp, .5f);
+
+    public void TankStartEngine(AnimationEvent e) => TankMotorSound();
+
+    public void TankStopEngine(AnimationEvent e) => tankOn = false;
+
+    private void Awake() {
+        tractorStartDuration = (double)tractorStart.samples / tractorStart.frequency;
+        input = GetComponentInParent<Character.IInput>();
+        characterController = GetComponentInParent<CharacterController>();
+    }
+
+    private bool tankOn;
+    private void TankMotorSound() {
+        tankOn = true;
+        idleSource.pitch = 1f;
+        idleSource.volume = idleVolume = 1f;
+        startSource.volume = 1f;
+        
+        Debug.Log("TANK START");
+        startSource.Play();
+        idleStartTime = AudioSettings.dspTime + tractorStartDuration;
+        // Queue tractorIdle to play when tractorStart ends
+        idleSource.PlayScheduled(idleStartTime);
+    }
+
+    private float idleVolume;
+    private void Update() {
+        // Lower engine volume. Make time for startUpSound to play
+        if (!tankOn && idleVolume > 0f) {
+            idleVolume -= Time.deltaTime * 1f / engineWindDownTime;
+            idleSource.volume = idleVolume;
+            startSource.volume = idleVolume;
+        }
+        if (!tankOn && idleVolume <= 0f && idleSource.isPlaying) {
+            idleSource.Stop();
+        }
+
+        if (tankOn) {
+            Vector3 velocity = characterController.velocity;
+            velocity.y = 0f;
+            float vel = velocity == Vector3.zero ? 0f : 1f;
+            idleSource.pitch = Mathf.Lerp(1f, engineThrottlePitch ,  Mathf.Min(vel, input.Axis.magnitude));
+        }
+    }
+}
