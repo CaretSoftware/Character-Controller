52f20c84bb97ca8ae5c200eda68f019213840148
CaretSoftware <69549081+CaretSoftware@users.noreply.github.com>
2023-11-10 01:32:36 +0100

    Nuclear Arsenal

---
+++ Assets/Scripts/AtomicBomb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class AtomicBomb : MonoBehaviour {
+    [SerializeField] private SphereCollider collider;
+    [SerializeField] private GameObject mushroomCloud;
+    
+    private void Awake() => Invoke(nameof(EnableCollider), 1f);
+
+    private void EnableCollider() => collider.enabled = true;
+
+    private void OnTriggerEnter(Collider other) {
+        // Explode
+        Instantiate(mushroomCloud, transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
+    }
+}
