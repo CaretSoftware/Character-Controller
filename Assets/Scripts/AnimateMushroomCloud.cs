3280b702897d8cf1e4d81f58a37875ef4f027174
CaretSoftware <69549081+CaretSoftware@users.noreply.github.com>
2023-11-11 06:54:22 +0100

    Nuclear Projectiles
    Cleaned up folder structure.
    Moved Animations and Scripts from the Asset folder

---
+++ Assets/Scripts/AnimateMushroomCloud.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class AnimateMushroomCloud : MonoBehaviour {
+    private static readonly int Explosion = Animator.StringToHash("Explode");
+    private static Coroutine dimLightsCoroutine;
+    
+    [SerializeField] private Animator myAnimator;
+    [SerializeField] private float setInactiveTime = 2.3f;
+    [SerializeField] private float dimmedIntensity = .5f;
+    [SerializeField] private float duration = 1f;
+    private Light directionalLight;
+    private float defaultIntensity;
+    
+    private void Awake() {
+        directionalLight = GameObject.Find("Directional Light").GetComponent<Light>();
+        defaultIntensity = directionalLight.intensity;
+    }
+
+    public void Explode() {
+        CancelInvoke();
+        this.gameObject.SetActive(true);
+        myAnimator.SetTrigger(Explosion);
+        SoundManager.PlaySound(Sound.SonicTinnitus, 2f);
+        Invoke(nameof(SetInactive), setInactiveTime);
+        if (dimLightsCoroutine != null)
+            StopCoroutine(dimLightsCoroutine);
+        dimLightsCoroutine = StartCoroutine(DimDirectionalLight());
+    }
+
+    private IEnumerator DimDirectionalLight() {
+        float t = 0f;
+        while (t < 1f) {
+            directionalLight.intensity = Mathf.Lerp(dimmedIntensity, defaultIntensity, Ease.InExpo(t));
+            t += Time.deltaTime * (1f / duration);
+            yield return null;
+        }
+        directionalLight.intensity = defaultIntensity;
+    }
+    
+    private void SetInactive() => this.gameObject.SetActive(false);
+}
---
+++ Assets/Scripts/BlastWave.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Character;
+using UnityEngine;
+
+public class BlastWave : MonoBehaviour {
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private float blastForce = 20f;
+    private Rigidbody[] rigidbodies;
+    private Transform mainCamera;
+    private bool played;
+    private float distance;
+    private HashSet<Rigidbody> affectedRigidbodies = new HashSet<Rigidbody>();
+    
+    private void OnEnable() {
+        affectedRigidbodies.Clear();
+        rigidbodies = FindObjectsOfType<Rigidbody>();
+        played = false;
+        if (mainCamera == null)
+            mainCamera = Camera.main.transform;
+        distance = (mainCamera.position - transform.position).magnitude;
+    }
+
+    private void Update() {
+        Vector3 position = transform.position;
+        float currentScale = transform.localScale.x;
+        for (int i = 0; i < rigidbodies.Length; i++) {
+            Vector3 dir = rigidbodies[i].transform.position - position;
+
+            if (affectedRigidbodies.Contains(rigidbodies[i]) || dir.magnitude > currentScale)
+                continue;
+            
+            rigidbodies[i].AddForce(blastForce * dir);
+            affectedRigidbodies.Add(rigidbodies[i]);
+        }
+        if (!played && currentScale >= distance - 10f) {
+            played = true;
+            SoundManager.PlaySound(Sound.BlastWave, 2f);
+        }
+    }
+}
